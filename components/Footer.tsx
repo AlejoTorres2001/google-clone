b3cb845de3d44827829911dda74b07c7029acce2
T0w3r <torres.alejo2001@gmail.com>
2022-01-05 12:49:14 -0300

    starting with footer

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+import { GlobeIcon } from "@heroicons/react/solid";
+
+const Footer = () => {
+    return (
+        <footer  className=" bottom-0 ">
+            <div>
+                <p>Argentina</p>
+            </div>
+            
+        </footer>
+    )
+}
+
+export default Footer
