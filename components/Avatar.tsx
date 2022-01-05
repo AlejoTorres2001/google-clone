eda6a9f2e2b7439d06d4d5b3bc8170da166f2b09
T0w3r <torres.alejo2001@gmail.com>
2022-01-05 11:57:30 -0300

    added avatar comp

---
+++ components/Avatar.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable @next/next/no-img-element */
+
+
+interface Props{
+    url: string
+}
+
+const Avatar = ({url}:Props) => {
+    return (
+        <img 
+        src={url}
+        loading="lazy"
+        alt="avatar"
+        className="rounded-full cursor-pointer transition duration-150 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+        height={40}
+        width={40}
+        >
+        </img>
+    )
+}
+
+export default Avatar
